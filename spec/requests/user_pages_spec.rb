537298309a002baac6126c7ade2b5010d497ad01
vt4a2h <vt4a2h@gmail.com>
2014-01-07 00:16:21 +0700

    finished signup/signin/signout

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -54,8 +54,14 @@ describe "User pages" do
         before { click_button submit }
         let(:user) { User.find_by(email: 'user@example.com') }
 
+        it { should have_link('Sign out') }
         it { should have_title(user.name) }
         it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+
+        describe "followed by signout" do
+          before { click_link "Sign out" }
+          it { should have_link('Sign in') }
+        end
       end
     end
   end
