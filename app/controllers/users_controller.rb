4c02e3f28d84afe3d0dfa6d470580d90611cc19e
vt4a2h <vt4a2h@gmail.com>
2014-01-07 18:06:51 +0700

    completed frendly forwarding

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -39,8 +39,11 @@ class UsersController < ApplicationController
       redirect_to(root_url) unless current_user?(@user)
     end
 
-  def signed_in_user
-      redirect_to signin_url, notice: "Please sign in." unless signed_in?
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
+      end
     end
 
     def user_params
