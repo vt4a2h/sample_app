5413a8156a812a660e006175f329474af29ee169
vt4a2h <vt4a2h@gmail.com>
2014-01-07 21:22:33 +0700

    finished user edit, update, index and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
-  before_action :signed_in_user, only: [:index, :edit, :update]
+  before_action :signed_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
 
   def index
     @users = User.paginate(page: params[:page], per_page: 10)
@@ -26,6 +27,12 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted."
+    redirect_to users_url
+  end
+
   def create
     @user = User.new(user_params)
     if @user.save
@@ -38,6 +45,11 @@ class UsersController < ApplicationController
   end
 
   private
+
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
+    end
+
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_url) unless current_user?(@user)
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -15,7 +15,7 @@ describe "User pages" do
     it { should have_content('All users') }
 
     describe "pagination" do
-      # TODO: need use let(:users_count) {10}... but in depricated in this version
+      # TODO: convenient to use let(:users_count) {10}... but in depricated in this version
       before(:all) { 10.times { FactoryGirl.create(:user) } }
       after(:all)  { User.delete_all }
 
@@ -25,6 +25,27 @@ describe "User pages" do
         User.paginate(page: 1, per_page: 10).each do |user|
           expect(page).to have_selector('li', text: user.name)
         end
+      end
+    end
+
+    describe "delete links" do
+
+      it { should_not have_link('delete') }
+
+      describe "as an admin user" do
+        let(:admin) { FactoryGirl.create(:admin) }
+        before do
+          sign_in admin
+          visit users_path
+        end
+
+        it { should have_link('delete', href: user_path(User.first)) }
+        it "should be able to delete another user" do
+          expect do
+            click_link('delete', match: :first)
+          end.to change(User, :count).by(-1)
+        end
+        it { should_not have_link('delete', href: user_path(admin)) }
       end
     end
   end
