fba291703310cd40b314ee1e572715bc0b64ab73
vt4a2h <vt4a2h@gmail.com>
2014-01-08 19:04:23 +0700

    added presentation microposts to the main page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,9 @@
 class StaticPagesController < ApplicationController
   def home
-    @micropost = current_user.microposts.build if signed_in?
+    if signed_in?
+      @micropost  = current_user.microposts.build
+      @feed_items = current_user.feed.paginate(page: params[:page], per_page: 10)
+    end
   end
 
   def help
