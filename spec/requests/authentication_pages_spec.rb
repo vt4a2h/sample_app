07b3dfef142167b4af4da4da10422935e12281fa
vt4a2h <vt4a2h@gmail.com>
2014-01-10 00:40:43 +0700

    added working flow button without ajax

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -54,6 +54,18 @@ describe "Authentication" do
           click_button "Sign in"
         end
 
+        describe "in the Relationships controller" do
+          describe "submitting to the create action" do
+            before { post relationships_path }
+            specify { expect(response).to redirect_to(signin_path) }
+          end
+
+          describe "submitting to the destroy action" do
+            before { delete relationship_path(1) }
+            specify { expect(response).to redirect_to(signin_path) }
+          end
+        end
+
         describe "after signing in" do
 
           it "should render the desired protected page" do
