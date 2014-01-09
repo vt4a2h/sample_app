94ed38b1bef5fab5afb00344163a889daa962e22
vt4a2h <vt4a2h@gmail.com>
2014-01-09 17:35:40 +0700

    added view for followers and followed

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 SampleApp::Application.routes.draw do
   get "micropost/create"
   get "micropost/destroy"
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :sessions,   only: [:new, :create, :destroy]
   resources :microposts, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
   root  'static_pages#home'
 
   match '/signup',  to: 'users#new',            via: 'get'
